74bad6db8974b4979e00b3f9fc5d8d4fbd9f018b
uk4pyc <56728913+uk4pyc@users.noreply.github.com>
2020-06-16 21:09:08 +0300

    master

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// Get modal element
+var modal = document.getElementById('simpleModal');
+// Get open modulation
+var burger = document.getElementById('burger');
+
+// Listen for open click
+burger.addEventListener('click', openModal);
+
+// Listen for outside click
+window.addEventListener('click', clickOutside);
+
+// Function to open modal
+function openModal(){
+    modal.style.display = 'block';    
+}
+
+// Function to close modal
+function closeModal(){
+    modal.style.display = 'none';    
+}
+
+// Function to close modal if outside click
+function clickOutside(e){
+    if(e.target == modal){
+        modal.style.display = 'none';    
+    }
+}
