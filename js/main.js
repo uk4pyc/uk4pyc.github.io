9c8d2158f23e1d646ec369940c4eb3b7b1bd1a6f
uk4pyc <56728913+uk4pyc@users.noreply.github.com>
2020-06-16 21:35:26 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,25 +3,35 @@ var modal = document.getElementById('simpleModal');
 // Get open modulation
 var burger = document.getElementById('burger');
 
-// Listen for open click
-burger.addEventListener('click', openModal);
-
-// Listen for outside click
-window.addEventListener('click', clickOutside);
-
-// Function to open modal
-function openModal(){
-    modal.style.display = 'block';    
+burger.onclick = function () {
+    modal.style.display = "block";
 }
 
-/* Function to close modal
-function closeModal(){
-    modal.style.display = 'none';    
-}*/
-
-// Function to close modal if outside click
-function clickOutside(e){
-    if(e.target == modal){
-        modal.style.display = 'none';    
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
     }
 }
+
+// // Listen for open click
+// burger.addEventListener('click', openModal);
+
+// // Listen for outside click
+// window.addEventListener('click', clickOutside);
+
+// // Function to open modal
+// function openModal(){
+//     modal.style.display = 'block';    
+// }
+
+// /* Function to close modal
+// function closeModal(){
+//     modal.style.display = 'none';    
+// }*/
+
+// // Function to close modal if outside click
+// function clickOutside(e){
+//     if(e.target == modal){
+//         modal.style.display = 'none';    
+//     }
+// }
